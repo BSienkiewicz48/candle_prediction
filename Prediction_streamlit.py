1815c2fc4f8497b3d737a406128c18f47a4b634c
BSienkiewicz48 <150445140+BSienkiewicz48@users.noreply.github.com>
2025-02-26 14:31:02 +0100

    kolor

--- Prediction_streamlit.py
+++ Prediction_streamlit.py
@@ -247,7 +247,8 @@ fig.add_trace(go.Candlestick(
     low=[pred_min_price[0]],
     close=[pred_close_price[0]],
     name='Predicted',
-    fillcolor='rgba(0, 0, 255, 0.5)',  
+    increasing_fillcolor='rgba(0, 0, 255, 0.5)',  
+    decreasing_fillcolor='rgba(0, 0, 255, 0.5)'   
 ))
 
 # Ustawienia wykresu
